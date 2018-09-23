a9c0a6ff27edb91d896deb9e6758c9d3d04ba5a3
Kausar Ahmed <kausarahmedpial@gmail.com>
2018-09-23 12:50:24 +0600

    Fixed About Tab

--- static/js/script.js
+++ static/js/script.js
@@ -212,27 +212,6 @@
 	});
 
 
-/* ========================================================================= */
-/*	On scroll fade/bounce effect
-/* ========================================================================= */
-	var scroll = new SmoothScroll('a[href*="#"]');
-
-/* ========================================================================= */
-	/*	Header Scroll Background Change
-	/* ========================================================================= */	
-	
-$(window).scroll(function() {    
-var scroll = $(window).scrollTop();
- //console.log(scroll);
-if (scroll > 200) {
-    //console.log('a');
-    $(".navigation").addClass("sticky-header");
-} else {
-    //console.log('a');
-    $(".navigation").removeClass("sticky-header");
-}});
-
-
 })(jQuery);
 
 
