d33aa4f4f212d4cc5897ad1ba9b111f42158aa1a
rguichard <rguichard@vsense.fr>
2020-01-03 18:01:38 +0100

    smoothscrolling
    - disable theme's plugin
    - enable a standalone javascript script

---
+++ static/plugins/smoothscroll.js
@@ -0,0 +1,17 @@
+    $(function() {
+        /**
+        * Smooth scrolling to page anchor on click
+        **/
+        $("a[href*='#']:not([href='#'])").click(function() {
+            if (
+                location.hostname == this.hostname
+                && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
+            ) {
+                var anchor = $(this.hash);
+                anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
+                if ( anchor.length ) {
+                    $("html, body").animate( { scrollTop: anchor.offset().top }, 1000);
+                }
+            }
+        });
+    });
