33d9d822f5ce547362166054c21b3a4fdf7b48d8
Kevin Lefevre <lefevre.kevin@gmail.com>
2020-01-20 14:10:06 +0100

    feat: add formspree contact email
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -174,7 +174,7 @@
 			/* using the jquery's post(ajax) function and a lifesaver
 			function serialize() which gets all the data from the form
 			we submit it to send_email.php */
-			$.post("sendmail.php", $("#contact-form").serialize(), function (result) {
+			$.post("https://formspree.io/mzbeyarl", $("#contact-form").serialize(), function (result) {
 				//and after the ajax request ends we check the text returned
 				if (result == 'sent') {
 					//if the mail is sent remove the submit paragraph
